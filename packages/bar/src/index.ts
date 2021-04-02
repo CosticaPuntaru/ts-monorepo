a86429d7c99b20113bbb5b5197e094dcd5f1c165
costica.puntaru <costica.puntaru@everymatrix.com>
2021-04-02 15:25:14 +0300

    add broken example of deep import a file from package

--- packages/bar/src/index.ts
+++ packages/bar/src/index.ts
@@ -1,4 +1,6 @@
 import { meaningOfLife } from "@nighttrax/foo";
+import { theDeepMeaningOfLife } from "@nighttrax/foo/utils/my-utils";
 
 // eslint-disable-next-line no-console
 console.log(meaningOfLife);
+console.log('theDeepMeaningOfLife', theDeepMeaningOfLife)
